f6de989efc30aaeec0628a0d7a3d28565115a442
Aditya Majethia <aditya@synerflow.in>
2025-10-02 23:22:57 +0530

    Emptied test directory.

--- test/colorfull_test.dart
+++ test/colorfull_test.dart
@@ -1,8 +1,1 @@
-import 'package:colorfull/colorfull.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Testing a single color', () {
-    pumpkinOrangeM700;
-  });
-}
+void main() {}
